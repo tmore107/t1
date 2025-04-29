d8866d78f176077e1c047f7547c213dd887d2fc9
tmore107 <moretrupti107@gmail.com>
2025-04-29 14:43:36 +0530

    commite trupti

--- GDemo/src/com/Add.java
+++ GDemo/src/com/Add.java
@@ -9,9 +9,14 @@ public class Add {
 	public void m2(){
 		System.out.println("This is m2 method()");
 	}
+
+	public void m3(){
+		System.out.println("This is m2 method()");
+	}
 	
 	public static void main(String[] args) {
 		System.out.println("This is main class");
                 System.out.println("Main method");
+		System.out.println("m3 method is added");
 	}
 }
