b4c8ce8f59014eef0d6ce8e1473768c542f8bd1e
Ajay Sathe <Ajay Sathe@LAPTOP-SCLIVQOO>
2025-04-29 14:37:41 +0530

    Commited by Trupti

---
+++ GDemo/src/com/Add.java
@@ -0,0 +1,17 @@
+package com;
+
+public class Add {
+	public void m1(){
+		System.out.println("This is m1 method()");
+		System.out.println("Demo method");
+	}
+
+	public void m2(){
+		System.out.println("This is m2 method()");
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("This is main class");
+                System.out.println("Main method");
+	}
+}
